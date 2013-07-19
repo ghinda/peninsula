9cb8a3dcf138639eea73cb8ce1c11987b61820e0
Ionut Colceriu <contact@ghinda.net>
2013-07-19 09:15:44 +0300

    Woohoo full support for starred items in Opera Mini that wasn't working previously.

--- js/base.js
+++ js/base.js
@@ -3,56 +3,56 @@ if (typeof window.localStorage == 'undefined' || typeof window.sessionStorage ==
 
 var Storage = function (type) {
   function createCookie(name, value, days) {
-	var date, expires;
+    var date, expires;
 
-	if (days) {
-	  date = new Date();
-	  date.setTime(date.getTime()+(days*24*60*60*1000));
-	  expires = "; expires="+date.toGMTString();
-	} else {
-	  expires = "";
-	}
-	document.cookie = name+"="+value+expires+"; path=/";
+    if (days) {
+      date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      expires = "; expires="+date.toGMTString();
+    } else {
+      expires = "";
+    }
+    document.cookie = name+"="+value+expires+"; path=/";
   }
 
   function readCookie(name) {
-	var nameEQ = name + "=",
-		ca = document.cookie.split(';'),
-		i, c;
+    var nameEQ = name + "=",
+        ca = document.cookie.split(';'),
+        i, c;
 
-	for (i=0; i < ca.length; i++) {
-	  c = ca[i];
-	  while (c.charAt(0)==' ') {
-		c = c.substring(1,c.length);
-	  }
+    for (i=0; i < ca.length; i++) {
+      c = ca[i];
+      while (c.charAt(0)==' ') {
+        c = c.substring(1,c.length);
+      }
 
-	  if (c.indexOf(nameEQ) == 0) {
-		return c.substring(nameEQ.length,c.length);
-	  }
-	}
-	return null;
+      if (c.indexOf(nameEQ) == 0) {
+        return c.substring(nameEQ.length,c.length).replace(/'/g, '"');
+      }
+    }
+    return null;
   }
-  
+
   function setData(data) {
-	data = JSON.stringify(data);
-	if (type == 'session') {
-	  window.name = data;
-	} else {
-	  createCookie('localStorage', data, 365);
-	}
+    data = JSON.stringify(data).replace(/"/g, "'");
+    if (type == 'session') {
+      window.name = data;
+    } else {
+      createCookie('localStorage', data, 365);
+    }
   }
-  
+
   function clearData() {
-	if (type == 'session') {
-	  window.name = '';
-	} else {
-	  createCookie('localStorage', '', 365);
-	}
+    if (type == 'session') {
+      window.name = '';
+    } else {
+      createCookie('localStorage', '', 365);
+    }
   }
-  
+
   function getData() {
-	var data = type == 'session' ? window.name : readCookie('localStorage');
-	return data ? JSON.parse(data) : {};
+    var data = type == 'session' ? window.name : readCookie('localStorage');
+    return data ? JSON.parse(data) : {};
   }
 
 
@@ -60,34 +60,34 @@ var Storage = function (type) {
   var data = getData();
 
   return {
-	length: 0,
-	clear: function () {
-	  data = {};
-	  this.length = 0;
-	  clearData();
-	},
-	getItem: function (key) {
-	  return data[key] === undefined ? null : data[key];
-	},
-	key: function (i) {
-	  // not perfect, but works
-	  var ctr = 0;
-	  for (var k in data) {
-		if (ctr == i) return k;
-		else ctr++;
-	  }
-	  return null;
-	},
-	removeItem: function (key) {
-	  delete data[key];
-	  this.length--;
-	  setData(data);
-	},
-	setItem: function (key, value) {
-	  data[key] = value+''; // forces the value to a string
-	  this.length++;
-	  setData(data);
-	}
+    length: 0,
+    clear: function () {
+      data = {};
+      this.length = 0;
+      clearData();
+    },
+    getItem: function (key) {
+      return data[key] === undefined ? null : data[key];
+    },
+    key: function (i) {
+      // not perfect, but works
+      var ctr = 0;
+      for (var k in data) {
+        if (ctr == i) return k;
+        else ctr++;
+      }
+      return null;
+    },
+    removeItem: function (key) {
+      delete data[key];
+      this.length--;
+      setData(data);
+    },
+    setItem: function (key, value) {
+      data[key] = value+''; // forces the value to a string
+      this.length++;
+      setData(data);
+    }
   };
 };
 
@@ -96,12 +96,12 @@ if (typeof window.sessionStorage == 'undefined') window.sessionStorage = new Sto
 
 })();
 
-/* Peninsula Unofficial 2012
- * 2012 Ionut Colceriu - ghinda.net
+/* Peninsula Unofficial 2013
+ * 2013 Ionut Colceriu - ghinda.net
  */
  
 /* Get favorites from localStorage */
-var starredArtists = localStorage.getItem('starredArtists');
+var starredArtists = window.localStorage.getItem('starredArtists');
 
 if(starredArtists) {
 	starredArtists = JSON.parse(starredArtists);
@@ -168,7 +168,7 @@ var starArtist = function(e) {
 	$button.toggleClass('starred');
 	
 	// update localStorage
-	localStorage.setItem('starredArtists', JSON.stringify(starredArtists));
+	window.localStorage.setItem('starredArtists', JSON.stringify(starredArtists));
 };
 
 // init
